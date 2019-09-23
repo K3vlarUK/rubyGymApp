323fd489cb25b334fa69a09149377ed40a917933
Kevin McDermott <kmcdermott31@gmail.com>
2019-09-23 13:36:53 +0100

    mode tidying up

--- controllers/member_controller.rb
+++ controllers/member_controller.rb
@@ -3,6 +3,8 @@ require('sinatra/contrib/all')
 require_relative('../models/member.rb')
 also_reload('../models/*')
 
+require('pry')
+
 get '/members' do
   @members = Member.all()
   erb (:"members/index")
