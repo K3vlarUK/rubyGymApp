7ebc5e1926c6c8d928afc8ff506556fda4c948da
Kevin McDermott <kmcdermott31@gmail.com>
2019-09-27 10:13:33 +0100

    added some JS for warnings

---
+++ public/JS/functions.js
@@ -0,0 +1,19 @@
+function deleteAlert() {
+  alert("Class has been deleted!");
+}
+
+function cancelledAlert() {
+  alert("Member cancelled from class!");
+}
+
+function  memberAdded() {
+  alert("Member has been added to the database.");
+}
+
+function  classAdded() {
+  alert("Class has been added to the database.");
+}
+
+function classBooked() {
+  alert("Member booked into class.");
+}
